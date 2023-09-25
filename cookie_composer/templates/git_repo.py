02bf112dcd1d951b3ac5c0d38a6295ce5aa72848
Corey Oordt <coreyoordt@gmail.com>
2023-09-25 10:41:48 -0500

    Added git template repositories.

---
+++ cookie_composer/templates/git_repo.py
@@ -0,0 +1,55 @@
+"""Utility functions for handling and fetching repo archives in git format."""
+from pathlib import Path
+from typing import Optional
+
+from cookiecutter.utils import make_sure_path_exists
+
+from cookie_composer.git_commands import checkout_ref, clone, get_repo
+from cookie_composer.templates.types import Locality, TemplateFormat, TemplateRepo
+
+
+def get_repo_name(repo_url: str, checkout: Optional[str] = None) -> str:
+    """Construct the destination repo name from the repo URL and checkout."""
+    repo_name = repo_url.rstrip("/").rsplit("/", 1)[-1]  # Get the last part of the URL path
+    repo_name = repo_name.rsplit(".git")[0]  # Strip off any .git suffix
+    if checkout is not None:
+        repo_name = f"{repo_name}_{checkout}"
+    return repo_name
+
+
+def template_repo_from_git(
+    git_uri: str, locality: Locality, cache_dir: Path, checkout: Optional[str] = None
+) -> TemplateRepo:
+    """
+    Return a template repo from a git URI.
+
+    - If the repo is remote, it will be cloned to the cache_dir and named after the repo and checkout value,
+    for example `mytemplate_main`. This allows for multiple versions of the same repo to be cached without
+    a conflict.
+    - If the repo is already cloned, it will not be cloned again.
+    - If the repo is local, it is not cloned but will check out `checkout`, and the local path is returned.
+
+    """
+    if locality == Locality.LOCAL:
+        ensure_clean = checkout is not None
+        repo = get_repo(git_uri, search_parent_directories=True, ensure_clean=ensure_clean)
+    else:
+        cache_dir = Path(cache_dir).expanduser().resolve()
+        make_sure_path_exists(cache_dir)
+        repo_name = get_repo_name(git_uri, checkout)
+        repo_dir = cache_dir.joinpath(repo_name)
+        repo = clone(git_uri, repo_dir)
+
+    repo.remotes.origin.pull()
+
+    if checkout:
+        checkout_ref(repo, checkout)
+
+    return TemplateRepo(
+        source=git_uri,
+        cached_source=Path(repo.working_dir),
+        format=TemplateFormat.GIT,
+        locality=locality,
+        checkout=checkout,
+        password=None,
+    )
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 """Testing configuration."""
+import shutil
 from pathlib import Path
 
 import pytest
@@ -12,15 +13,47 @@ def fixtures_path() -> Path:
 
 
 @pytest.fixture
-def default_repo(tmp_path):
-    """Make a bunch of default commits to a temporary bare git repo."""
-    origin = Repo.init(tmp_path / "origin", bare=True)
-    origin.index.commit(
+def default_origin(tmp_path: Path) -> Repo:
+    """Create a default origin repo."""
+    tmp_repo_path = tmp_path / "tmp_repo"
+    tmp_repo = Repo.init(tmp_repo_path)
+    tmp_repo_path.joinpath("README.md").write_text("Hello World!")
+    tmp_repo.index.add(["README.md"])
+    tmp_repo.index.commit(
         message="new: first commit", committer=Actor("Bob", "bob@example.com"), commit_date="2022-01-01 10:00:00"
     )
-    origin.create_head("remote-branch")
+    tmp_repo_path.joinpath("README.md").write_text("Hello World!\n\nThis is a test.")
+    tmp_repo.index.add(["README.md"])
+    tmp_repo.index.commit(
+        message="second commit", committer=Actor("Bob", "bob@example.com"), commit_date="2022-01-01 11:00:00"
+    )
+    tmp_repo.create_tag("v1.0.0")
+    tmp_repo.create_head("remote-branch")
+    tmp_repo.heads["remote-branch"].checkout()
+    tmp_repo_path.joinpath("newfile.md").write_text("Hello World!\n\nThis is a test.\n\nThis is a new line.")
+    tmp_repo.index.add(["newfile.md"])
+    tmp_repo.index.commit(
+        message="first commit on a branch",
+        committer=Actor("Bob", "bob@example.com"),
+        commit_date="2022-01-01 12:00:00",
+    )
+    origin_path = tmp_path / "origin.git"
+    origin = Repo.init(origin_path, bare=True)
 
-    repo = origin.clone(tmp_path / "repo")
+    tmp_repo.create_remote("origin", str(origin_path))
+    tmp_repo.remotes.origin.push("master")
+    tmp_repo.remotes.origin.push("remote-branch")
+    tmp_repo.remotes.origin.push("v1.0.0")
+
+    shutil.rmtree(tmp_repo_path)
+    return origin
+
+
+@pytest.fixture
+def default_repo(default_origin: Repo, tmp_path: Path) -> Repo:
+    """Clone the default origin repo."""
+    repo = default_origin.clone(tmp_path / "repo")
     repo.heads.master.checkout()
     repo.remotes.origin.pull()
+
     return repo
