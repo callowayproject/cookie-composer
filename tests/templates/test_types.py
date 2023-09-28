1c0c05454f97068414a7e1155de126ad65aa1878
Corey Oordt <coreyoordt@gmail.com>
2023-09-28 12:28:36 -0500

    Moved `get_template_name` tests

---
+++ tests/templates/test_types.py
@@ -0,0 +1,32 @@
+"""Tests for the template types module."""
+import pytest
+from pytest import param
+
+from cookie_composer.templates.types import get_template_name
+
+
+@pytest.mark.parametrize(
+    ["value", "expected"],
+    [
+        param("/path/to/template/", "template", id="local directory"),
+        param("/path/to/composition.yaml", "composition", id="local composition"),
+        param("https://example.com/path/to/template", "template", id="remote directory"),
+        param("https://example.com/path/to/composition.yaml", "composition", id="remote composition"),
+    ],
+)
+def test_get_template_name(value: str, expected: str):
+    """The template name should be the base name of the path."""
+    assert get_template_name(value) == expected
+
+
+@pytest.mark.parametrize(
+    ["bad_value"],
+    [
+        ("https://example.com",),
+        ("",),
+    ],
+)
+def test_get_template_name_errors(bad_value):
+    """It should raise errors."""
+    with pytest.raises(ValueError):
+        get_template_name(bad_value)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,15 +5,15 @@ import pytest
 from pytest import param
 
 from cookie_composer import utils
-from cookie_composer.composition import read_rendered_composition
+from cookie_composer.io import read_rendered_composition
 
 
-def test_get_context_for_layer(fixtures_path):
+def test_get_context_for_layer(fixtures_path: Path):
     """Return a context for a given layer."""
     rendered_comp = read_rendered_composition(fixtures_path / "rendered_composition.yaml")
 
     result1 = utils.get_context_for_layer(rendered_comp, 0)
-    assert result1 == rendered_comp.layers[0].new_context
+    assert result1 == rendered_comp.layers[0].rendered_context
 
     result2 = utils.get_context_for_layer(rendered_comp, 1)
     assert "project_slug" in result2
@@ -25,33 +25,6 @@ def test_get_context_for_layer(fixtures_path):
 
     result4 = utils.get_context_for_layer(rendered_comp)
     assert result4 == result3
-
-
-@pytest.mark.parametrize(
-    ["value", "expected"],
-    [
-        param("/path/to/template/", "template", id="local directory"),
-        param("/path/to/composition.yaml", "composition", id="local composition"),
-        param("https://example.com/path/to/template", "template", id="remote directory"),
-        param("https://example.com/path/to/composition.yaml", "composition", id="remote composition"),
-    ],
-)
-def test_get_template_name(value: str, expected: str):
-    """The template name should be the base name of the path."""
-    assert utils.get_template_name(value) == expected
-
-
-@pytest.mark.parametrize(
-    ["bad_value"],
-    [
-        ("https://example.com",),
-        ("",),
-    ],
-)
-def test_get_template_name_errors(bad_value):
-    """It should raise errors."""
-    with pytest.raises(ValueError):
-        utils.get_template_name(bad_value)
 
 
 def test_remove_single_path(tmp_path: Path):
